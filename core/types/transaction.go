bc59a0f3671bc47efcd47b5b3284cf9b621cedb8
Viktor <kirillovmr@gmail.com>
2023-08-24 15:51:02 +0600

    Update transaction.go with Arbitrum Tx Types

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -45,6 +45,13 @@ const (
 	AccessListTxType = 0x01
 	DynamicFeeTxType = 0x02
 	BlobTxType       = 0x03
+
+	ArbitrumDepositTxType         = 0x64
+	ArbitrumUnsignedTxType        = 0x65
+	ArbitrumContractTxType        = 0x66
+	ArbitrumRetryTxType           = 0x68
+	ArbitrumSubmitRetryableTxType = 0x69
+	ArbitrumInternalTxType        = 0x6A
 )
 
 // Transaction is an Ethereum transaction.
